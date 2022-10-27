1b6c1f99c1744913c2807b8e8acf7fd91ee68125
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-27 17:46:49 -0400

    start adding player name form

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-/*  */
+/* 
+Clean up the interface to allow players to put in their names, include a button to start/restart the game and add a display element that congratulates the winning player!
+*/
 
 // gameBoard
 const gameBoard = (() => {
