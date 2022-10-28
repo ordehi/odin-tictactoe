1ce52e489751f1cdd647c2d0f5abaed18e478317
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-28 06:55:57 -0400

    display score, reset game, restart game

--- game.js
+++ game.js
@@ -1,22 +1,72 @@
 /* 
 Clean up the interface to allow players to put in their names, include a button to start/restart the game and add a display element that congratulates the winning player!
 */
+const playerInput = document.getElementById('playerInput');
+const gameControls = document.getElementById('gameControls');
+const pxDisplay = gameControls.querySelector('#pxDisplay');
+const poDisplay = gameControls.querySelector('#poDisplay');
+
+//displayController
+const displayController = (() => {
+  const renderBoard = (doc, board) => {
+    const display = doc.querySelector('#gameDisplay');
+
+    [...display.children].map((cell, idx) => {
+      let played = board[idx] || '';
+      cell.textContent = played;
+    });
+  };
+
+  const updateScores = (playerX, playerO) => {
+    pxDisplay.textContent = `${playerX.name}: ${playerX.score}`;
+    poDisplay.textContent = `${playerO.name}: ${playerO.score}`;
+  };
+
+  const togglePlayerInput = (pInput) => {
+    pInput.classList.toggle('hidden');
+    pInput.onclick =
+      pInput.onclick ||
+      function (e) {
+        if (e.target.classList.contains('commit'))
+          inputController.handlePlayerInput(
+            e.target.parentElement.parentElement.parentElement
+          );
+      };
+  };
+
+  const toggleGameControls = (gControls) => {
+    gControls.classList.toggle('hidden');
+    gControls.onclick =
+      gControls.onclick ||
+      function (e) {
+        if (e.target.tagName.toLowerCase() === 'button')
+          inputController.handleControls(e.target);
+      };
+  };
+
+  return {
+    renderBoard,
+    togglePlayerInput,
+    toggleGameControls,
+    updateScores,
+  };
+})();
 
 // gameBoard
 const gameBoard = (() => {
   // implement an event emitter on state change call renderBoard
-  const _board = [];
+  let _board = [];
 
   const read = () => _board;
 
   const write = (mark, idx, displayController) => {
     _board[idx] = mark;
-    displayController.renderBoard(document);
+    displayController.renderBoard(document, _board);
   };
 
   const reset = (displayController) => {
     _board = [];
-    displayController.renderBoard(document);
+    displayController.renderBoard(document, []);
   };
 
   return {
@@ -26,43 +76,33 @@ const gameBoard = (() => {
   };
 })();
 
-//displayController
-const displayController = (() => {
-  const renderBoard = (doc) => {
-    const board = gameBoard.read();
-    const display = doc.querySelector('#gameDisplay');
-
-    board.map((cell, idx) => {
-      let cellNode = display.children[idx];
-      cellNode.textContent = cell;
-    });
-  };
-
-  return {
-    renderBoard,
-  };
-})();
-
 // gameController
 const gameController = (() => {
   let _over = false;
-  const _game = {};
+  let _game = {};
   let _turn = 'X';
   const _winningCombos = [
     [0, 1, 2],
-    [2, 3, 4],
-    [5, 6, 7],
+    [3, 4, 5],
+    [6, 7, 8],
     [0, 4, 8],
     [0, 3, 6],
     [1, 4, 7],
     [2, 4, 6],
     [2, 5, 8],
-    [6, 7, 8],
   ];
 
   const addPlayers = (playerX = 'PlayerX', playerO = 'PlayerO') => {
     _game['X'] = { name: playerX, score: 0, plays: [] };
     _game['O'] = { name: playerO, score: 0, plays: [] };
+    displayController.updateScores(_game['X'], _game['O']);
+  };
+
+  const init = () => {
+    _over = false;
+    _game = {};
+    _turn = 'X';
+    displayController.togglePlayerInput(playerInput);
   };
 
   const changeTurn = () => {
@@ -87,7 +127,11 @@ const gameController = (() => {
     let winner = Object.keys(result);
 
     if (winner.length) {
-      console.log(`Winner is ${winner[0]} with ${result[winner[0]]}`);
+      console.log(
+        `Winner is ${_game[winner[0]].name} with ${result[winner[0]]}`
+      );
+      _game[winner[0]].score = ++_game[winner[0]].score || 1;
+      displayController.updateScores(_game['X'], _game['O']);
       _over = true;
     }
     changeTurn();
@@ -112,15 +156,17 @@ const gameController = (() => {
   };
 
   const startGame = () => {
-    gameBoard.reset();
+    gameBoard.reset(displayController);
+    _over = false;
+    _turn = 'X';
+    _game['X'].plays = [];
+    _game['O'].plays = [];
   };
 
   const resetScore = () => {
-    if (Object.keys(_game).length) {
-      for (player in _game) {
-        _game[player].score = 0;
-      }
-    }
+    _game['X'].score = 0;
+    _game['O'].score = 0;
+    displayController.updateScores(_game['X'], _game['O']);
   };
 
   const resetGame = () => {
@@ -129,11 +175,33 @@ const gameController = (() => {
   };
 
   return {
-    addPlayers,
+    init,
     play,
     startGame,
     resetGame,
     checkWinner,
+    addPlayers,
+  };
+})();
+
+const inputController = (() => {
+  const handlePlayerInput = (form) => {
+    gameController.addPlayers(form.player1.value, form.player2.value);
+    displayController.togglePlayerInput(playerInput);
+    displayController.toggleGameControls(gameControls);
+  };
+
+  const handleControls = (eventTarget) => {
+    if (eventTarget.id === 'restart') {
+      gameController.startGame();
+    } else if (eventTarget.id === 'reset') {
+      gameController.resetGame();
+    }
+  };
+
+  return {
+    handlePlayerInput,
+    handleControls,
   };
 })();
 
@@ -149,4 +217,4 @@ const Player = (name, score) => {
   return { name, score };
 };
 
-gameController.addPlayers();
+gameController.init();
